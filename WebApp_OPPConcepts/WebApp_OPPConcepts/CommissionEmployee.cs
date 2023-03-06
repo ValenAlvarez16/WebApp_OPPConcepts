f7e08836101222febfbf32f4487938a490d1593f
VALENTINA ALVAREZ PANIAGUA <valentinaalvarez305940@correo.itm.edu.co>
2023-03-05 22:47:50 -0500

    New CommissionEmployee.cs class created

---
+++ WebApp_OPPConcepts/WebApp_OPPConcepts/CommissionEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp_OPPConcepts
+{
+    public class CommissionEmployee : Employee // salary hereda de Employee que es el papá por eso los ":"
+    {
+
+        #region Properties
+
+        public double CommissionPercentage { get; set; }
+
+        public decimal Sales { get; set; }
+
+
+        #endregion
+
+
+        public override decimal GetValueToPay()
+        {
+            return Sales * ((decimal)CommissionPercentage/100);//esto es un casteo 
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t" + //base es la palabra reservada para reutilizar lo que hay en Employee
+                   $"Commission Percentage.................{CommissionPercentage/100:P2}\n\t" + //P es de porcentaje
+                   $"Sales.................................{Sales:C2}\n\t" + //c quiere decir plata
+                   $"Get Value To Pay......................{GetValueToPay():C2}\n\t";
+        }
+
+      
+    }
+}
--- WebApp_OPPConcepts/WebApp_OPPConcepts/Program.cs
+++ WebApp_OPPConcepts/WebApp_OPPConcepts/Program.cs
@@ -15,19 +15,19 @@ namespace WebApp_OPPConcepts
                 Console.WriteLine("***************");
                 Console.WriteLine("*My first Date Class*");
                 Console.WriteLine("***************");
-                Console.WriteLine("");
+                Console.WriteLine(" ");
 
                 Console.WriteLine("Please type your year: ");
                 int year = Convert.ToInt32(Console.ReadLine());
                 Console.ReadKey();
 
-                Console.WriteLine("");
+                Console.WriteLine(" ");
 
                 Console.WriteLine("Please type your month: ");
                 int month = Convert.ToInt32(Console.ReadLine());
                 Console.ReadKey();
 
-                Console.WriteLine("");
+                Console.WriteLine(" ");
 
                 Console.WriteLine("Please type your day: ");
                 int day = Convert.ToInt32(Console.ReadLine());
@@ -37,6 +37,7 @@ namespace WebApp_OPPConcepts
                 var dateObject = new Date(year, month, day); // todo lo que yo haga y mande, me lo mada con un toString
                                                              // al menos que yo empiece a desarrollar la lógica
                 Console.Write("The date entered is: " + dateObject);
+                Console.WriteLine(" ");
 
 
                 //traerme la información de la clase Employee
@@ -53,7 +54,21 @@ namespace WebApp_OPPConcepts
 
                 };
                 Console.WriteLine(salaryEmployee); //para leer los datos
+                Console.WriteLine(" ");
 
+                Employee commissionEmployee = new CommissionEmployee()
+                {
+                    Id = 1,
+                    FirstName = "Julio",
+                    LastName = "Pérez",
+                    BirthDate = dateObject,
+                    HiringDate = dateObject,
+                    IsActive = false,
+                    CommissionPercentage = 5,
+                    Sales = 20000000
+
+                };
+                Console.WriteLine(commissionEmployee);
 
             }
             catch (Exception message)
